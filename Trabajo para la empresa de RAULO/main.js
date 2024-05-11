893ff72e1b92e069a8a918876fadca62165c6173
Jolsman01 <javierformosanm@gmail.com>
2024-05-10 23:33:31 -0300

    Add files via upload

---
+++ Trabajo para la empresa de RAULO/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+    this.document.getElementById("Camiseta").addEventListener("click", function () {
+        var colorFondo = document.getElementById("Camiseta").valve;
+    });
+});
+
