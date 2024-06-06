68d8f670bab8c71d54633222eae5d6d7a2d6d258
Jolsman01 <javierformosanm@gmail.com>
2024-06-06 12:31:26 -0300

    primer avance en nosotros
    Hay que arreglar cuadro de reporte y terminar nosotros

--- Trabajo para la empresa de RAULO/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-    this.document.getElementById("Camiseta").addEventListener("click", function () {
-        var colorFondo = document.getElementById("Camiseta").valve;
-    });
-});
-
